b171046afd9caad3df81b30b5f980f5e2800a4a9
svmatthews <matthews.sam@gmail.com>
2014-05-23 14:14:46 -0700

    first files

---
+++ README.md
@@ -0,0 +1,21 @@
+Instagram Responsive Blocks
+===========================
+
+This library is intended for some quick-usage instagram feed work. The library allows you specify how many images wide and tall you want your feed to be within your specified container.
+
+## Setup
+
+1. Include the `instares.js` and `instares.css` in your project's `<head>`
+
+```HTML
+<link href="css/instares.css" rel="stylesheet">
+<script type="text/javascript" src="js/instares.js"></script>
+```
+
+2. Initiate the instagram feed via JavaScript
+
+```JS
+instablocks({
+	// options
+});
+```
---
+++ responsive-instagram.js
@@ -0,0 +1,37 @@
+function insta(e) {
+	var c = e.container !== undefined ? e.container : error('container');
+	var key = e.client_id !== undefined ? e.client_id : error('client_id');
+	var w = e.width !== undefined ? e.width : 5;
+	var h = e.height !== undefined ? e.height : 2;
+	console.log(c, w, h);
+	makeBlocks(c,key,w,h);
+}
+
+function makeBlocks(c,key,w,h) {
+	tot = w*h;
+	var container = document.getElementById(c)
+	$.ajax({
+		type: "GET",
+		dataType: "jsonp",
+		cache: false,
+		url: 'https://api.instagram.com/v1/users/257720515/media/recent/?client_id='+key+'&count='+tot,
+		success: function(d) {
+			for(i=0;i<tot;i++) {
+				var block = document.createElement('div');
+				block.className='insta-block';
+				block.style.width=100/w+'%';
+				block.style['padding-bottom']=100/w+'%';
+				block.style['background-image']='url('+d.data[i].images.standard_resolution.url+')';
+				container.appendChild(block);
+			}
+		},
+		error: function(xhr, ajaxOptions, thrownError) {
+			console.log(xhr.status, thrownError);
+		}
+	});
+}
+
+function error(message) {
+	this.message = message;
+	this.name = 'FatalError';
+}
