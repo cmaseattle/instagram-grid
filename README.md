ab72b091b1a50feb0c81c2d75799cba94a442095
svmatthews <matthews.sam@gmail.com>
2014-05-27 15:25:32 -0700

    version 0.0.2

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 Instagram Responsive Grid
 =========================
 
-This library is intended for some quick-usage instagram feed work. The library allows you specify how many images wide and tall you want your feed to be within your specified container. **Requires jQuery** for `ajax` call.
+version **0.0.2**
+
+This library is intended for some quick-usage instagram feed work. Instagram-Grid allows you specify how many images wide and tall you want your feed to be within your specified container. **Requires jQuery** for `ajax` call. *NOTE*: maximum number of images in a single call is *33 images*.
 
 ## Setup
 
@@ -28,7 +30,7 @@ instagramGrid({
 **client_id**   
 `client_id` defines your client ID obtained from the instagram API. No authentication needed, just a quick API key. Follow [these steps](https://github.com/svmatthews/instagram-access-token-generation).
 
-## Option Parameters
+## Optional Parameters
 
 **width**   
 `width` in number of images you wish to span the horizontal space of your defined `container`.   
@@ -36,11 +38,15 @@ Default is 5.
 
 **height**   
 `height` in number of images you wish to span the vertical space of your defined `container`.   
-Default is 2. 
+Default is 2.
+
+**link**
+`link` will add clickability to your images, allowing you to click and see a larger version of the image within your site - without linking to Instagram.   
+Default is `false`
 
 ## Example
 
-Initiate the instagram blocks to span 6 images across and 3 images down within the `#social` element.
+Initiate the instagram blocks to span 6 images across and 3 images down within the `#social` element. Clicking the element will show a larger, lightbox version of your image.
 
 ```JS
 $(document).ready(function(){
@@ -48,18 +54,32 @@ $(document).ready(function(){
 		container: 'social',
 		client_id: 'your-client-id',
 		width: 6,
-		height: 3
+		height: 3,
+		link: true
 	});
 });
 ```
 
 ## Roadmap
 
-* remove jQuery reliance
+* remove jQuery reliance for ajax call
 * build error handler
 * more optional parameters
-	* linkable options
-	* linkable within current page (build lightbox) or linkable to instagram website
+	* -linkable options-
+	* -linkable within current page (build lightbox)- or linkable to instagram website
 * stack images vertically at specific media queries (optional)
 * -build testing/development environment-
 * add example `gh-pages` branch with user input fields
+# -start recording history-
+* *build this as a real javascript library, rather than a file of accessible functions*
+
+## History
+
+**0.0.2**
+
+* added `link` parameter with lightbox - default is `false`
+* passing object from initial parameters reduces function-specific parameters and increases readability
+
+**0.0.1** - Original Release 5/23/2014
+
+* testing environment with simple function-based library
--- src/instagram-grid.js
+++ src/instagram-grid.js
@@ -1,29 +1,25 @@
 function instagramGrid(e) {
-	var c = e.container !== undefined ? e.container : error('container');
-	var key = e.client_id !== undefined ? e.client_id : error('client_id');
-	var w = e.width !== undefined ? e.width : 5;
-	var h = e.height !== undefined ? e.height : 2;
-	console.log(c, w, h);
-	makeBlocks(c,key,w,h);
+	var w = e.width !== undefined ? e.width : 5,
+			h = e.height !== undefined ? e.height : 2,
+			options = {
+				'container': e.container !== undefined ? e.container : error('container'),
+				'key': e.client_id !== undefined ? e.client_id : error('client_id'),
+				'width': w,
+				'height': h,
+				'total': w*h,
+				'link': e.link !== undefined ? e.link : false
+			};
+	getImages(options);
 }
 
-function makeBlocks(c,key,w,h) {
-	tot = w*h;
-	var container = document.getElementById(c);
+function getImages(o) {
 	$.ajax({
 		type: "GET",
 		dataType: "jsonp",
 		cache: false,
-		url: 'https://api.instagram.com/v1/users/257720515/media/recent/?client_id='+key+'&count='+tot,
+		url: 'https://api.instagram.com/v1/users/257720515/media/recent/?client_id='+o.key+'&count='+o.total,
 		success: function(d) {
-			for(i=0;i<tot;i++) {
-				var block = document.createElement('div');
-				block.className='insta-block';
-				block.style.width=100/w+'%';
-				block.style['padding-bottom']=100/w+'%';
-				block.style['background-image']='url('+d.data[i].images.standard_resolution.url+')';
-				container.appendChild(block);
-			}
+			makeBlocks(d,o);
 		},
 		error: function(xhr, ajaxOptions, thrownError) {
 			console.log(xhr.status, thrownError);
@@ -31,7 +27,41 @@ function makeBlocks(c,key,w,h) {
 	});
 }
 
+function makeBlocks(d,o) {
+	console.log(d);
+	for(i=0;i<o.total;i++) {
+		var block = document.createElement('div');
+		var container = document.getElementById(o.container);
+		block.id='image-'+i;
+		block.className='insta-block';
+		if(o.link) {
+			block.className+=' clickable';
+			block.addEventListener('click', function(){
+				var imageContain = document.createElement('div');
+				imageContain.className = 'instagram-cover';
+				imageContain.addEventListener('click', function(){
+					this.parentNode.removeChild(this);
+				});
+				var id = parseInt(this.id.slice(6), 10);
+				var image = document.createElement('div');
+				image.className='insta-block-large';
+				image.style['background-image']='url('+d.data[id].images.standard_resolution.url+')';
+				image.innerHTML='<div class="insta-block-large-close">&times;</div>';
+				
+				// append new elements to each other and to body
+				imageContain.appendChild(image);
+				document.body.appendChild(imageContain);
+			});
+		}
+		block.style.width=100/o.width+'%';
+		block.style['padding-bottom']=100/o.width+'%';
+		block.style['background-image']='url('+d.data[i].images.low_resolution.url+')';
+		container.appendChild(block);
+	}
+}
+
 function error(message) {
 	this.message = message;
 	this.name = 'FatalError';
 }
+
